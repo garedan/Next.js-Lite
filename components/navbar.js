821772abfcda52bf3a32356c4053bff6fb8595ed
garedan <danyG22@proton.me>
2023-06-11 00:33:41 -0300

    Added next.config.js support

--- components/navbar.js
+++ components/navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
             <ul>
                 <li>
                     <Link href="/">
-                        <a className='m-auto p-5 underline'>Garedan - Next.js Lite + Simple Movie Catalog</a>
+                        <a className='m-auto p-5 underline'>2023-{process.env.myCustomEnvironment}</a>
                     </Link>
                 </li>
                 {/* <li>
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        myCustomEnvironment: 'Garedan - Next.js Lite + Simple Movie Catalog from next.config.js',
+  },
+}
+
+module.exports = nextConfig
